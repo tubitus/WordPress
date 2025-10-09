b0888e6c92dfa7996c3089bdaffffd9e79a9e6f1
Jakub Vlček <Jakub.Vlcek@student.souepl.cz>
2025-10-09 11:22:08 +0200

    footer vlky

--- wp-content/themes/Updraft/footer.php
+++ wp-content/themes/Updraft/footer.php
@@ -1,8 +1,24 @@
-   
-        <footer>
-            <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/footer.css">
-            &copy; <?php echo date('Y'); ?> Updraft. All rights reserved.
-        </footer>
-        <?php wp_footer(); ?>
-        </body>
-        </html>
+<footer class="footer">
+    <div>
+    <p class="footer-text">&copy; <?php echo date('Y'); ?> Updraft. All rights reserved.</p>
+    <!-- SVG for waves animation -->
+    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <!-- Defining SVG element for waves -->
+      <defs>
+        <!-- Defining the wave path once to reuse it for creating multiple wave layers -->
+        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g class="wave-parallax">
+        <!-- Group of waves that will move at different speeds to create a parallax effect -->
+        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(0, 0, 0, 0.7)" /> <!-- First wave layer -->
+        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(85, 85, 85, 0.5)" /> <!-- Second wave layer -->
+        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(105, 105, 105, 0.3)" /> <!-- Third wave layer -->
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="#8080816b" /> <!-- Fourth wave layer -->
+      </g>
+    </svg>
+  </div>
+</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/Updraft/page.php
+++ wp-content/themes/Updraft/page.php
@@ -1,9 +1,29 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<?php $theme_url = get_template_directory_uri(); ?>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<main>
-    <?php
-        the_content();
-    ?>
-</main>
+         <script src="<?= $theme_url?>/script/script.js" defer></script>
+         <link rel="stylesheet" href="<?php $theme_url ?>/style.css">
+         <link rel="stylesheet" href="<?php $theme_url ?>/style/footer.css">
+         <?php wp_head(); ?>
+    </head>
+    <body>
+        <?php
+            echo  $theme_url. "/script/script.js"; // Výpis cesty k šabloně pro kontrolu
+            get_header();
+        ?>
 
-<?php get_footer(); ?>
+        <main id="primary" class="site-main">
+            <?php
+                the_content();
+            ?>
+        </main>
+
+        <?php
+            get_footer();
+        ?>
+    </body>
+</html>
